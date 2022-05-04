74c57d1ba47ffc0e6a5e3712bdef6d370acab1c3
jorgeoliverat <jorgeoliverat@gmail.com>
2022-05-03 23:18:39 -0500

    Se agrega el proyecto Navbar

---
+++ proyectoreactjs/src/App.js
@@ -0,0 +1,12 @@
+
+import NavBar from './Components/NavBar/NavBar';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <NavBar/>
+  );
+}
+
+export default App;
